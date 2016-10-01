b470ab044ad56ee47a92a355d6addfbc2380390f
Robin <rob00x@gmail.com>
2016-10-01 14:30:33 +0100

    Add preliminary analysis and loading scripts

---
+++ R/load-all-data.R
@@ -0,0 +1,10 @@
+# Aim: load all schools data
+private_dir = "S:/Faculty-of-Medicine-and-Health/Research-Projects-2/CDRC/NPCT/DR160129_02"
+old = setwd(private_dir) 
+list.files() # what's there
+sld15 = readr::read_tsv(file = "SLD_CENSUS_2015.txt")
+sld15 # look at the data
+sld11 = readr::read_tsv(file = "SLD_CENSUS_2011.txt")
+s11 = readr::read_tsv(file = "Spring_Census_2011.txt")
+s10 = readr::read_tsv(file = "Spring_Census_2010.txt")
+setwd(old) # go back to working directory
