9921c24af5cbed99ab4e878722ff2e697de6ddc8
ilanfri <ilanfri@mac.com>
2016-10-04 17:44:07 +0100

    Insignificant changes.

--- R/subset-data.R
+++ R/subset-data.R
@@ -18,8 +18,8 @@ bbox(cents_lsoa)
 cents_lsoa = spTransform(cents_lsoa, CRSobj = proj4string(las))
 
 # subset the od data
-sel_od = s11$LLSOA_SPR11 %in% cents_lsoa$LSOA11CD
-s = s11[sel,]
+sel_od = s11$LLSOA_SPR11 %in% cents_lsoa$LSOA11CD 
+s = s11[sel_od,]
 s = s[!is.na(s$LLSOA_SPR11),]
 names(s)
 
@@ -27,3 +27,11 @@ cents_lsoa = cents_lsoa[leeds_la,]
 plot(leeds_la)
 points(cents_lsoa)
 flow = od2line(flow = s, zones = cents_lsoa, destinations = sld_leeds)
+
+numcoords = merge(x = s, y = sld_leeds, by.x = "URN_SPR11", by.y = "LEA11_URN", all.x = TRUE)
+numcoords
+
+numcoords1 = merge(x = numcoords, y = cents_lsoa, by.x = "LLSOA_SPR11", by.y = "LSOA11CD", all.x = TRUE)
+nrow(numcoords1)
+
+numcoords1 = numcoords1[,c("LLSOA_SPR11","URN_SPR11","SCHOOLNAME_SPR11","Secondary","TOTAL","CAR","CYCLE","WALK","OTHER","UNKNOWN","coords.x1.x","coords.x1.y","coords.x2.x","coords.x2.y")]
--- setup.R
+++ setup.R
@@ -1,5 +1,7 @@
 # Aim: load packages to analyse travel to school data
 # source("https://gist.githubusercontent.com/jbryer/9112634/raw/29c0a82659ed303c9fa03f1ec07d86161294974d/package.R")
-pkgs = c("devtools", "dplyr", "readr", "tmap", "sp", "rgeos")
-# package(pkgs)
+pkgs = c("devtools", "dplyr", "readr", "sp", "rgeos", "tmap", "stplanr")
+to_install = !pkgs %in% installed.packages()
+if(sum(to_install) > 0)
+  install.packages(pkgs[to_install])
 lapply(X = pkgs, FUN = library, character.only = T)
