abbcff388be3652fccd1b18825b13ee10384e23a
Robin <rob00x@gmail.com>
2016-10-04 17:50:34 +0100

    Update scripts to show travel to school across

--- R/subset-data.R
+++ R/subset-data.R
@@ -5,25 +5,33 @@ las = readRDS("../pct-bigdata/las.Rds")
 leeds_la = las[las$NAME == "Leeds",]
 plot(leeds_la)
 proj4string(sld_sp) = proj4string(las)
-sld_leeds = sld_sp[leeds_la,]
-points(sld_leeds)
-saveRDS(sld_leeds, "private_data/sld_leeds.Rds")
+schools = sld_sp[leeds_la,]
+points(schools)
+saveRDS(schools, "private_data/sld_leeds.Rds")
 
 # subset all ods that go to these schools
 
 # lsoa
 cents_lsoa = readRDS("../pct-bigdata/cents_lsoa.Rds")
+schools = readRDS("private_data/sld_leeds.Rds")
 plot(cents_lsoa)
 bbox(cents_lsoa)
 cents_lsoa = spTransform(cents_lsoa, CRSobj = proj4string(las))
+cents_lsoa = cents_lsoa[leeds_la,]
 
 # subset the od data
 sel_od = s11$LLSOA_SPR11 %in% cents_lsoa$LSOA11CD
-s = s11[sel,]
+s = s11[sel_od,]
 s = s[!is.na(s$LLSOA_SPR11),]
+s = s[s$URN_SPR11 %in% schools$LEA11_URN,]
 names(s)
+names(cents_lsoa)[1] = names(s)[1]
+names(schools)[2] = names(s)[3]
+summary(s$LLSOA_SPR11 %in% cents_lsoa$LLSOA_SPR11)
+summary(s$URN_SPR11 %in% schools$URN_SPR11)
 
-cents_lsoa = cents_lsoa[leeds_la,]
 plot(leeds_la)
 points(cents_lsoa)
-flow = od2line(flow = s, zones = cents_lsoa, destinations = sld_leeds)
+flow = od2line(flow = s, zones = cents_lsoa, destinations = schools)
+flow = flow[flow$TOTAL > 10,]
+plot(flow)
--- setup.R
+++ setup.R
@@ -1,5 +1,5 @@
 # Aim: load packages to analyse travel to school data
 # source("https://gist.githubusercontent.com/jbryer/9112634/raw/29c0a82659ed303c9fa03f1ec07d86161294974d/package.R")
-pkgs = c("devtools", "dplyr", "readr", "tmap", "sp", "rgeos")
+pkgs = c("devtools", "dplyr", "readr", "tmap", "sp", "rgeos", "stplanr")
 # package(pkgs)
 lapply(X = pkgs, FUN = library, character.only = T)
