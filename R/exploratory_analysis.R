1371ace16dd08f080e58050bb21b9e9fc50265af
ilanfri <ilanfri@mac.com>
2016-11-03 17:18:46 +0000

    Correlation plots fixed

--- R/exploratory_analysis.R
+++ R/exploratory_analysis.R
@@ -1,7 +1,9 @@
 setwd("/home/geoif/pct/pctSchoolsUK")
 source("setup.R")
-if(!exists("sld11"))
-  source("R/analysis-sld.R")
+
+# read in the data
+#if(!exists("sld11"))
+#  source("R/analysis-sld.R")
 
 # s11 contains the 2011 flows (origin-destination)
 # sld11 contains the information on the schools (destinations) for 2011
@@ -9,41 +11,45 @@ if(!exists("sld11"))
 
 # We ignored the 2015 data (sld15) as no corresponding flows (s15) exist for it
 
+# read in the raw data
 private_dir = "private_data"
 old = setwd(private_dir) 
-list.files() # what's there
+#list.files() # what's there
 sld11 = readr::read_tsv(file = "SLD_CENSUS_2011.txt")
 s11 = readr::read_tsv(file = "Spring_Census_2011.txt")
 setwd(old) # go back to working directory
 
+# fix column names and variable types
 names(sld11) = gsub("^LEA11_","", names(sld11))
 sld11$Northing = as.integer(sld11$Northing)
 
-vname_primary_f = paste0("FT_Girls_", 1:11)
-vname_primary_m = paste0("FT_Boys_", 1:11)
-vname_primary = c(vname_primary_f, vname_primary_m)
-sld11$Headcount_Primary_male = rowSums(sld11[,vname_primary_m])
-sld11$Headcount_Primary_female = rowSums(sld11[,vname_primary_f])
-sld11$Headcount_Primary = rowSums(sld11[,vname_primary])
-
-vname_secondary_f = paste0("FT_Girls_", 12:18)
-vname_secondary_m = paste0("FT_Boys_", 12:18)
-vname_secondary = c(vname_secondary_f, vname_secondary_m)
-sld11$Headcount_Secondary_male = rowSums(sld11[,vname_secondary_m])
-sld11$Headcount_Secondary_female = rowSums(sld11[,vname_secondary_f])
-sld11$Headcount_Secondary = rowSums(sld11[,vname_secondary])
+# extract the Primary schools
+# vname_primary_f = paste0("FT_Girls_", 1:11)
+# vname_primary_m = paste0("FT_Boys_", 1:11)
+# vname_primary = c(vname_primary_f, vname_primary_m)
+# sld11$Headcount_Primary_male = rowSums(sld11[,vname_primary_m])
+# sld11$Headcount_Primary_female = rowSums(sld11[,vname_primary_f])
+# sld11$Headcount_Primary = rowSums(sld11[,vname_primary])
+# 
+# # extract the Secondary schools
+# vname_secondary_f = paste0("FT_Girls_", 12:18)
+# vname_secondary_m = paste0("FT_Boys_", 12:18)
+# vname_secondary = c(vname_secondary_f, vname_secondary_m)
+# sld11$Headcount_Secondary_male = rowSums(sld11[,vname_secondary_m])
+# sld11$Headcount_Secondary_female = rowSums(sld11[,vname_secondary_f])
+# sld11$Headcount_Secondary = rowSums(sld11[,vname_secondary])
 
 # Load Phase of Education data so we can select Secondary schools only
-#install.packages("gdata", dependencies = TRUE)
-# gdata::installXLSXsupport()
 # https://www.whatdotheyknow.com/request/list_of_all_schools_in_england_w
 phase_edu = readxl::read_excel("phase_of_education.xls")
 #names(phase_edu)
 #names(sld11)
 
-nrow(sld11)
+before = nrow(sld11)
 sld11 = dplyr::inner_join(sld11, phase_edu, by = c("URN" = "URN"))
 nrow(sld11)
+# 5% of schools lost because we have no phase of education info for them
+before/nrow(sld11)
 
 unique(sld11$Phase)
 
@@ -53,16 +59,20 @@ nrow(sld11)
 sldpri = sld11[(sld11$Phase == "Primary") | (sld11$Phase == "Middle Deemed Primary"), ]
 nrow(sldpri)
 
-sum(sldpri$Headcount_Primary)
-sum(sldpri$Headcount_Primary)/sum(sld11$Headcount_Pupils)
+#sum(sldpri$Headcount_Primary)
+sum(sldpri$Headcount_Pupils)
+#sum(sldpri$Headcount_Primary)/sum(sld11$Headcount_Pupils)
+sum(sldpri$Headcount_Pupils)/sum(sld11$Headcount_Pupils)
 
 #nrow(sld11)
 #sldsec = sld11[(sld11$Phase == "Secondary") | (sld11$Phase == "Middle Deemed Secondary") | (sld11$Headcount_Secondary > 0), ]
 sldsec = sld11[(sld11$Phase == "Secondary") | (sld11$Phase == "Middle Deemed Secondary"), ]
 nrow(sldsec)
 
-sum(sldsec$Headcount_Secondary)
-sum(sldsec$Headcount_Secondary)/sum(sld11$Headcount_Pupils)
+#sum(sldsec$Headcount_Secondary)
+sum(sldsec$Headcount_Pupils)
+#sum(sldsec$Headcount_Secondary)/sum(sld11$Headcount_Pupils)
+sum(sldsec$Headcount_Pupils)/sum(sld11$Headcount_Pupils)
 
 ### For Primary and Secondary schools work out gender ratios, and Full-Time vs Part-Time numbers
 names(sld11)
@@ -81,6 +91,12 @@ isTRUE((sum(sld11$Headcount_Boys) + sum(sld11$Headcount_Girls)) == sum(sld11$Hea
 sum(sld11$Headcount_Boys)/sum(sld11$Headcount_Pupils)
 sum(sld11$Headcount_Girls)/sum(sld11$Headcount_Pupils)
 
+sum(sldpri$Headcount_Boys)/sum(sldpri$Headcount_Pupils)
+sum(sldpri$Headcount_Girls)/sum(sldpri$Headcount_Pupils)
+
+sum(sldsec$Headcount_Boys)/sum(sldsec$Headcount_Pupils)
+sum(sldsec$Headcount_Girls)/sum(sldsec$Headcount_Pupils)
+
 gendbins = sld11[, c("Headcount_Boys", "Headcount_Girls")]
 gendbins$Headcount_Boys = sum(gendbins$Headcount_Boys)
 gendbins$Headcount_Girls = sum(gendbins$Headcount_Girls)
@@ -89,9 +105,9 @@ gendbins = gendbins[1,]
 barplot(as.matrix(gendbins))
 
 
-par_orig = par()
-par()[["mar"]]
-par(mar=c(7.1,4.1,4.1,2.1))
+#par_orig = par()
+#par()[["mar"]]
+#par(mar=c(7.1,4.1,4.1,2.1))
 
 # Language
 sum(sld11$Pupils_Language_English)
@@ -113,11 +129,10 @@ barplot(as.matrix(langbins), main="Language")
 #barplot(langbins)
 
 # Region
-barplot(table(sld11$GORName), las=2)
+#barplot(table(sld11$GORName), las=2)
 
 library(ggplot2)
-ggplot(sld, aes(x=GORName)) + geom_bar() + theme(axis.text.x = element_text(angle=-90, vjust=0.5, hjust=0), axis.title.x = element_text("Region"))
-
+#ggplot(sld, aes(x=GORName)) + geom_bar() + theme(axis.text.x = element_text(angle=-90, vjust=0.5, hjust=0), axis.title.x = element_text("Region"))
 ggplot(sld11, aes(x=Headcount_Pupils)) + geom_histogram(binwidth = 100) #+ stat_function(fun = dnorm)
 
 # Fit Poisson distribution
@@ -127,10 +142,8 @@ ggplot(sld11, aes(x=Headcount_Pupils)) + geom_histogram(binwidth = 100) #+ stat_
 #normalfit = sapply(sld11$Headcount_Pupils, dnorm, mean=mean(sld11$Headcount_Pupils), sd=sd(sld11$Headcount_Pupils))
 #ggplot(sld11, aes(x=Headcount_Pupils)) + stat_bin(aes(y=..density..)) + geom_line(aes(x=Headcount_Pupils, y=normalfit), colour="red")
 
-sum(sld11$Pupil_Ethnicty_Unc)
-
 # Get breakdown by Ethnicity
-# Language English or not English
+sum(sld11$Pupil_Ethnicty_Unc)
 
 # Full-Time vs Part-Time split
 sum(sld11$Headcount_FT_Pupils)
@@ -138,22 +151,10 @@ sum(sld11$Headcount_PT_Pupils)
 
 # Religious split
 table(sld11$Denomination)
+nrow(sld11[sld11$Denomination != "Does not apply", ]) # Religious schools
+nrow(sld11[sld11$Denomination == "Does not apply", ]) # Secular schools
 
-# for(i in names(sld11)){
-#   if(is.character(sld11[[i]]) & length(levels(as.factor(sld11[[i]])))<100){
-#     print(paste0("Column:   ",i))
-#     print(unique(sld11[[i]]))
-#     cat("\n")
-#   }
-#   else if(!(grepl("FT_",i) | grepl("PT_",i) | grepl("Num_",i) | grepl("Pct_",i))){
-#     print(paste0("Column:   ",i))
-#     print(summary(sld11[[i]]))
-#     cat("\n")
-#   }
-# }
-
-
-str(sld11, list.len=ncol(sld11))
+#str(sld11, list.len=ncol(sld11))
 
 # Convert categorical columns into factors
 for(i in names(sld11)){
@@ -161,6 +162,8 @@ for(i in names(sld11)){
     sld11[[i]] = as.factor(sld11[[i]])
   }
 }
+
+str(sld11, list.len=ncol(sld11))
 
 # Get summary statistics for all the columns
 for(i in names(sld11)){
@@ -218,7 +221,8 @@ library(tmap)
 #qtm(lads_active, fill = c("Walk", "Cycle"), free.scales.fill=FALSE)
 qtm(lads_active, fill = c("Walk", "Cycle"))
 
-pairs(~Cycle+Walk, data=res_active)
+#pairs(~Cycle+Walk, data=res_active)
+plot(res_active$Cycle, res_active$Walk, xlab="Cycling commutes to school (%)", ylab="Walking commutes to school (%)")
 
 lad_pcycle$CODE = as.character(lad_pcycle$CODE)
 schoolvswork = inner_join(res_active_la, as.data.frame(lad_pcycle), by=c("LAD11CD"="CODE"))
