72eb2e7c8536bceaf10112e0b86827f63228f8c6
Robin Lovelace <rob00x@gmail.com>
2017-01-17 20:58:01 +0000

    Update data gen file

--- R/datagen.R
+++ R/datagen.R
@@ -10,7 +10,7 @@ las = readRDS("private_data/las_2011.Rds")
 cents_lsoa = readRDS("private_data/cents_lsoa_2011.Rds")
 cents_lsoa_leeds = cents_lsoa[grepl("Leeds", cents_lsoa$LSOA11NM),]
 las_leeds = las[cents_lsoa_leeds,]
-l = readRDS("private_data/Data_to_be_subset_for_Shiny_app.Rds")
+# l = readRDS("private_data/Data_to_be_subset_for_Shiny_app.Rds")
 # rf@data = cbind(l@data, rf@data)
 
 rf$bicycle = round(runif(n = nrow(rf), min = 0, max = 3))
@@ -18,9 +18,18 @@ rf$govtarget_slc = rf$bicycle * 2
 rf$dutch_slc = rf$bicycle * 10
 # rf = rf[rf$TOTAL > 5,] # cut at 5 for fast processing
 proj4string(rf)
-rnet_bicycle = overline(sldf = rf, attrib = "bicycle", fun = sum)
-# plot(rnet, lwd = rnet$bicycle / mean(rnet$bicycle))
-# rnet = rnet[rnet$bicycle > 5,] # cut if needs be
+rnet = overline(sldf = rf, attrib = "bicycle", fun = sum)
+rnet_govtarget = overline(sldf = rf, attrib = "govtarget_slc", fun = sum)
+rnet_dutch = overline(sldf = rf, attrib = "dutch_slc", fun = sum)
+rnet$govtarget_slc = rnet_govtarget$govtarget_slc
+rnet$dutch_slc = rnet_dutch$dutch_slc
+
+plot(rnet, lwd = rnet$dutch_slc / mean(rnet$bicycle))
+plot(rnet, lwd = rnet$govtarget_slc / mean(rnet$bicycle), add = T, col = "white")
+plot(rnet, lwd = rnet$bicycle / mean(rnet$bicycle), add = T, col = "red")
+summary(rnet$bicycle)
+rnet = rnet[rnet$bicycle > 5,] # cut if needs be
+saveRDS(rnet, "pctSchoolsApp/rf_leeds_schools.Rds")
 
 # use points method
 devtools::install_github("robinlovelace/stplanr", ref = "sfr-dep")
